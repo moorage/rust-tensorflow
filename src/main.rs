c69e4385fca25b3ed8912f8285df2fdb763c5e8a
Matthew Moore <m3moore@gmail.com>
2018-05-24 10:47:20 -0700

    got tensors compiling, now working on image reading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(feature = "nightly", feature(alloc_system))]
 #[cfg(feature = "nightly")]
 extern crate alloc_system;
+extern crate image;
 extern crate tensorflow;
 
 use std::error::Error;
@@ -19,11 +20,22 @@ use tensorflow::StepWithGraph;
 use tensorflow::Tensor;
 
 const MASK_RCNN_PB: &str = "/Users/mattmoore/Projects/rust-tensorflow/data/mask_rcnn.pb";
+const IMG_256SQ: &str = "/Users/mattmoore/Projects/rust-tensorflow/data/example.png";
 // const TF_MASKRCNN_TOILET_CLASSNUM: i32 = 1;
-const TF_MASKRCNN_IMG_WIDTHHEIGHT: i32 = 256;
+const TF_MASKRCNN_IMG_WIDTHHEIGHT: f32 = 256f32;
 const TF_MASKRCNN_IMAGE_METADATA_LENGTH: u64 = 10;
-const TF_MASKRCNN_IMAGE_METADATA: [i32; 10] = [ 0 ,TF_MASKRCNN_IMG_WIDTHHEIGHT ,TF_MASKRCNN_IMG_WIDTHHEIGHT , 3 , 0 , 0 ,TF_MASKRCNN_IMG_WIDTHHEIGHT ,TF_MASKRCNN_IMG_WIDTHHEIGHT , 0 , 0 ];
-
+const TF_MASKRCNN_IMAGE_METADATA: [f32; 10] = [
+    0f32,
+    TF_MASKRCNN_IMG_WIDTHHEIGHT,
+    TF_MASKRCNN_IMG_WIDTHHEIGHT,
+    3f32,
+    0f32,
+    0f32,
+    TF_MASKRCNN_IMG_WIDTHHEIGHT,
+    TF_MASKRCNN_IMG_WIDTHHEIGHT,
+    0f32,
+    0f32,
+];
 
 fn main() {
     // Putting the main code in another function serves two purposes:
@@ -43,17 +55,26 @@ fn run() -> Result<(), Box<Error>> {
         return Err(Box::new(
             Status::new_set(
                 Code::NotFound,
-                &format!(
-                    "Couldn't find model at {}.",
-                    MASK_RCNN_PB
-                ),
+                &format!("Couldn't find model at {}.", MASK_RCNN_PB),
             ).unwrap(),
         ));
     }
 
     // Create input variables for our addition
-    let input_img = <Tensor<f32>>::new(&[1, TF_MASKRCNN_IMG_WIDTHHEIGHT as u64, TF_MASKRCNN_IMG_WIDTHHEIGHT as u64, 3]);
-    let input_meta = Tensor::new(&[1, TF_MASKRCNN_IMAGE_METADATA_LENGTH]).with_values(&TF_MASKRCNN_IMAGE_METADATA).unwrap();
+    let opened_img = image::open(&IMG_256SQ)?;
+    // let img =
+    // let (img_w, img_h) = img.dimensions();
+    let img_float: Vec<f32> = opened_img.as_rgb8().unwrap().into_raw().iter().map(|&p| p as f32).collect();
+    let input_img = <Tensor<f32>>::new(&[
+        1,
+        TF_MASKRCNN_IMG_WIDTHHEIGHT as u64,
+        TF_MASKRCNN_IMG_WIDTHHEIGHT as u64,
+        3,
+    ]).with_values(&img_float)
+        .unwrap();
+    let input_meta = <Tensor<f32>>::new(&[1, TF_MASKRCNN_IMAGE_METADATA_LENGTH])
+        .with_values(&TF_MASKRCNN_IMAGE_METADATA)
+        .unwrap();
 
     // Load the computation graph defined by regression.py.
     let mut graph = Graph::new();
@@ -64,9 +85,18 @@ fn run() -> Result<(), Box<Error>> {
 
     // Run the Step
     let mut step = StepWithGraph::new();
-    step.add_input(&graph.operation_by_name_required("input_image")?, 0, &input_img);
-    step.add_input(&graph.operation_by_name_required("input_image_meta")?, 0, &input_meta);
-    let detections = step.request_output(&graph.operation_by_name_required("output_detections")?, 0);
+    step.add_input(
+        &graph.operation_by_name_required("input_image")?,
+        0,
+        &input_img,
+    );
+    step.add_input(
+        &graph.operation_by_name_required("input_image_meta")?,
+        0,
+        &input_meta,
+    );
+    let detections =
+        step.request_output(&graph.operation_by_name_required("output_detections")?, 0);
     let masks = step.request_output(&graph.operation_by_name_required("output_mrcnn_mask")?, 0);
     session.run(&mut step)?;
 
